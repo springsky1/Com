372391fffb51a231c4cf95f15db6b4799a1e1583
291077901@qq.com <291077901@qq.com>
2020-08-05 15:19:43 +0800

    DataTableToExcel

--- Lib/Npoi/NPOIHelper.cs
+++ Lib/Npoi/NPOIHelper.cs
@@ -5,6 +5,7 @@ using NPOI.XWPF.UserModel;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,9 +16,21 @@ namespace Lib.Npoi
     public class NPOIHelper
     {
 
-        public static void ExportExcel(DataTable dataTable)
+        public static void ExportExcelTest(DataTable dataTable)
         {
-            HSSFWorkbook workbook = new HSSFWorkbook();
+
+            MemoryStream memoryStream = ExportExcel(dataTable);
+
+            using (FileStream stream = File.OpenWrite(@"D:\mycode\HUBCom\Com\ConsoleApp1\Excel\" + DateTime.Now.Ticks.ToString() + ".xls"))
+            {
+                byte[] vs = memoryStream.ToArray();
+                stream.Write(vs, 0, vs.Length);
+            }
+        }
+
+        public static MemoryStream ExportExcel(DataTable dataTable)
+        {
+            IWorkbook workbook = new HSSFWorkbook();
             ISheet sheet = workbook.CreateSheet("Sheet1");
 
             IRow fristRow = sheet.CreateRow(0);
@@ -34,23 +47,30 @@ namespace Lib.Npoi
                 IRow row = sheet.CreateRow(i + 1);
                 DataRow rowData = dataTable.Rows[0];
 
-                // ICell cell = row.Cells.Add("");
 
-                //List<ICell> cells1 = row.Cells.AddRange((IEnumerable<ICell>)rowData.ItemArray);//.AsQueryable().Select(o => new ICell { cells2.Cells. } o.ToString())
+                for (int j = 0; j < dataTable.Columns.Count; j++)
+                {
+                    row.CreateCell(j).SetCellValue(dataTable.Rows[i][j].ToString());
 
-                List<ICell> cells1 = ((List<ICell>)(IEnumerable<ICell>)rowData.ItemArray);//.AsQueryable().Select(o => new ICell { cells2.Cells. } o.ToString())
-
-                //  row.Cells.AddRange(cells1);
+                }
             }
 
+            MemoryStream stream = new MemoryStream();
+            workbook.Write(stream);
+            return stream;
+
+            //using (FileStream stream = File.OpenWrite(@"D:\mycode\HUBCom\Com\ConsoleApp1\Excel\" + DateTime.Now.Ticks.ToString() + ".xls"))
+            //{
+            //    workbook.Write(stream);
+            //}
         }
 
         public static void Test()
         {
             DataTable dataTable = new DataTable();
             dataTable.Columns.Add("ID", typeof(String));
-            dataTable.Columns.Add("AGE", typeof(Int));
-            dataTable.Columns.Add("USERNAME", typeof(Int));
+            dataTable.Columns.Add("AGE");
+            dataTable.Columns.Add("USERNAME");
             dataTable.Columns.Add("ADDRESS", typeof(String));
             dataTable.Columns.Add("BIRTHDAY", typeof(String));
 
@@ -60,6 +80,8 @@ namespace Lib.Npoi
                 dataRow.ItemArray = new object[] { i.ToString(), (int)10, "测试" + i, $"山海路{i}号", "2020-01-01" };
                 dataTable.Rows.Add(dataRow);
             }
+
+            ExportExcelTest(dataTable);
 
         }
 
