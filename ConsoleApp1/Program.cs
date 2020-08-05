18e7b53f262705af671f6156a4bc022df85dda14
291077901@qq.com <291077901@qq.com>
2020-08-05 13:14:32 +0800

    12

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Lib;
+using Lib.Npoi;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,10 +14,10 @@ namespace ConsoleApp1
 
             //    RedisHelper.Test();
 
-            MongodbHelper mongodb = new MongodbHelper();
-            mongodb.Test();
+            // MongodbHelper mongodb = new MongodbHelper();
+            // mongodb.Test();
 
-
+            NPOIHelper.Test();
         }
     }
 }
--- Lib/Npoi/NPOIHelper.cs
+++ Lib/Npoi/NPOIHelper.cs
@@ -1,12 +1,14 @@
 ﻿using NPOI.HSSF.UserModel;
 using NPOI.SS.Formula.Functions;
 using NPOI.SS.UserModel;
+using NPOI.XWPF.UserModel;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ICell = NPOI.SS.UserModel.ICell;
 
 namespace Lib.Npoi
 {
@@ -18,15 +20,28 @@ namespace Lib.Npoi
             HSSFWorkbook workbook = new HSSFWorkbook();
             ISheet sheet = workbook.CreateSheet("Sheet1");
 
-            IRow cells = sheet.CreateRow(0);
+            IRow fristRow = sheet.CreateRow(0);
 
             ///设置头
             for (int i = 0; i < dataTable.Columns.Count; i++)
             {
                 var column = dataTable.Columns[i];
-                cells.CreateCell(i).SetCellValue(column.ColumnName);
+                fristRow.CreateCell(i).SetCellValue(column.ColumnName);
             }
 
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                IRow row = sheet.CreateRow(i + 1);
+                DataRow rowData = dataTable.Rows[0];
+
+                // ICell cell = row.Cells.Add("");
+
+                //List<ICell> cells1 = row.Cells.AddRange((IEnumerable<ICell>)rowData.ItemArray);//.AsQueryable().Select(o => new ICell { cells2.Cells. } o.ToString())
+
+                List<ICell> cells1 = ((List<ICell>)(IEnumerable<ICell>)rowData.ItemArray);//.AsQueryable().Select(o => new ICell { cells2.Cells. } o.ToString())
+
+                //  row.Cells.AddRange(cells1);
+            }
 
         }
 
@@ -42,7 +57,7 @@ namespace Lib.Npoi
             for (int i = 0; i < 10000; i++)
             {
                 DataRow dataRow = dataTable.NewRow();
-                dataRow.ItemArray = new string[] { i.ToString(), "10", "测试" + i, $"山海路{i}号", "2020-01-01" };
+                dataRow.ItemArray = new object[] { i.ToString(), (int)10, "测试" + i, $"山海路{i}号", "2020-01-01" };
                 dataTable.Rows.Add(dataRow);
             }
 
