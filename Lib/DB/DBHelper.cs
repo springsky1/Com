b952880988caa475889126bca4e93250672adfcf
291077901@qq.com <291077901@qq.com>
2020-08-12 09:39:00 +0800

    sql

---
+++ Lib/DB/DBHelper.cs
@@ -0,0 +1,23 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib.DB
+{
+    public class DBHelper
+    {
+        public static void Test()
+        {
+            SqlSugarClient client = new SqlSugarClient(new ConnectionConfig
+            {
+                ConnectionString = ""
+
+            });
+        }
+
+    }
+}
